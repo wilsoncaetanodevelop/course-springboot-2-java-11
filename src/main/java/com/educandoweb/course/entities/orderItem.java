7af12989b47a7838e031890715c4beb7afbd27de
wilsoncaetanodevelop <wilson-caaetano@hotmail.com>
2020-07-13 16:14:39 -0300

    OrderItem, many-to-many association with extra attributes Parte 1

---
+++ src/main/java/com/educandoweb/course/entities/orderItem.java
@@ -0,0 +1,92 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.educandoweb.course.entities.pk.OrderItemPk;
+
+
+@Entity
+@Table(name = "tb_order_item")
+public class orderItem implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private OrderItemPk id;
+	
+	private Integer quantite;
+	private Double price;
+
+	public orderItem() {
+	}
+
+	public orderItem(Order order, Product product, Integer quantite, Double price) {
+		super();
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantite = quantite;
+		this.price = price;
+	}
+
+	public Order getOrder() {
+		return id.getOrder();
+	}
+
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+
+	public Product getProduct() {
+		return id.getProduct();
+	}
+
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+
+	public Integer getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(Integer quantite) {
+		this.quantite = quantite;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		orderItem other = (orderItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
